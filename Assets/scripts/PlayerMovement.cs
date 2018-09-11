ab4cd6955cffda0d48dd60170915d0789265ed8a
Paul Atkins <patkins2@uncc.edu>
2018-09-11 00:38:43 -0400

    first commit
    First attempt. Game registers mouse click and attempts to move, but never really makes it there.

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    //these will allow you to change the values in the unity editor
+    [SerializeField] private Transform trans;
+    [SerializeField] private Rigidbody rb;
+
+	private float moveSpeed;
+	private float turnSpeed;
+
+	private void Start () {
+		moveSpeed = 5f;
+		turnSpeed = 5f;
+
+    }
+
+	private void Update () {
+
+		Vector3 inputVector = Vector3.zero;
+      
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            inputVector += Vector3.MoveTowards(trans.position, Input.mousePosition, 1000f);
+            //Debug.Log("Click");
+        }
+
+		// Normalize input vector to standardize movement speed
+		inputVector.Normalize ();
+		inputVector *= moveSpeed;
+		rb.velocity = inputVector;
+
+		// Face player along movement vector
+		Quaternion targetRotation = Quaternion.LookRotation (inputVector);
+
+		trans.rotation = Quaternion.Lerp (trans.rotation, targetRotation, turnSpeed * Time.deltaTime);
+	}
+}
