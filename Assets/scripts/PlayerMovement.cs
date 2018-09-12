49fcfcc7e36ba53899ed65bf66147ec7d20a0442
Paul Atkins <patkins2@uncc.edu>
2018-09-12 01:09:12 -0400

    player doesn't float but sinks
    I have now acheived the opposite affect. Instead of floating, the character now sinks halfway into the ground. Has something to do with line 34 in PlayerMovement.cs

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -25,32 +25,32 @@ public class PlayerMovement : MonoBehaviour {
 
         if(Input.GetMouseButtonDown(0))
         {
+            //set up the variable that will get the position of the mouse through the camera
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			RaycastHit hit;
 
-			if (Physics.Raycast (ray, out hit, 100f)) {
+			if (Physics.Raycast (ray, out hit, 100f)) { //this checks if the ray intersected with any objects i.e. the ray (mouse position), what it hit, and the maximum distance
 			
-				mousePosition = (hit.point + Vector3.up);
+				mousePosition = (hit.point + Vector3.forward);
 
-				towards = mousePosition - trans.position;
+				towards = mousePosition - trans.position; //the vector in which the player needs to travel
 			
 			}
-            //Debug.Log("Click");
+            //Debug.Log("Click"); this was just to check if the game would detect a click. 
         }
-		// Face player along movement vector
+		// This will make the player face the direction that it is traveling
 		Quaternion targetRotation = Quaternion.LookRotation(towards);
 
 		trans.rotation = Quaternion.Lerp (trans.rotation, targetRotation, turnSpeed * Time.deltaTime);
 
 			if(Vector3.Distance(trans.position, mousePosition) > radOfSat)
 			{
+                // this will normalize the vector to get the direction
 				towards.Normalize();
 				towards = towards * (moveSpeed * Time.deltaTime);
-
-				transform.position += towards;
-				
-			}
-
-		
+                
+                // this will move the player
+				trans.position += towards;
+			}		
 	}
 }
